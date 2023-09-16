def855af42c93c40fb34e4161e46dd47b18c3556
Christian Contarino <ccontarino@gmail.com>
2023-09-16 02:21:00 -0300

    fix: delete modal state

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -5,4 +5,3 @@ export const dateSelected = atom({
   date: new Date().toISOString().split('T')[0],
   isHoliday: false,
 });
-export const modalOpen = atom({ isOpen: false, bodyText: '', headerText: '' });
