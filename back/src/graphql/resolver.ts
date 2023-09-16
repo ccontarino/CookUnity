a3e316718bf828268da67b40c3e4c6cea1878706
Christian Contarino <ccontarino@gmail.com>
2023-09-16 02:12:24 -0300

    feat: add types

--- back/src/graphql/resolver.ts
+++ back/src/graphql/resolver.ts
@@ -2,14 +2,20 @@ import { deliveries } from '../data/deliverIes';
 import { holidayList } from '../data/holidays';
 import { HolidayListResponse } from '../types/holiday.types';
 
-export async function fetchDeliveries({ date }: any): Promise<any[]> {
+export async function fetchDeliveries({
+  date,
+}: {
+  date: string;
+}): Promise<any[]> {
   return deliveries.filter((delivery) => {
     return delivery.date === date;
   });
 }
 export async function fetchHolidayList({
   dateList,
-}: any): Promise<HolidayListResponse[]> {
+}: {
+  dateList: String[];
+}): Promise<HolidayListResponse[]> {
   return dateList.map((date: any) => {
     return { date, isHoliday: holidayList[date] || false };
   });
